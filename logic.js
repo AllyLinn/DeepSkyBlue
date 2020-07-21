3f9fb514d0571b7676e4e3a9f494ddb139b55b4c
Avery-Sichel <67975036+Avery-Sichel@users.noreply.github.com>
2020-07-21 15:53:50 -0400

    Update logic.js

--- logic.js
+++ logic.js
@@ -31,7 +31,11 @@ display computer array(animation controller JS)
 var gameInfo = {
     gameCount: 0, 
     maxTimer: 300,
-    currentTimer=300,
+    currentTimer: 300,
+    sound1: "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3",
+    sound2: "https://s3.amazonaws.com/freecodecamp/simonSound2.mp3",
+    sound3: "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3",
+    sound4: "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"
 }
 
 let userList = [];
