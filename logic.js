1ad6048437214cf7958ec29f5b806f10a0b4e583
Avery-Sichel <67975036+Avery-Sichel@users.noreply.github.com>
2020-07-21 14:44:38 -0400

    added links

---
+++ logic.js
@@ -0,0 +1,5 @@
+//sound files -->
+https://s3.amazonaws.com/freecodecamp/simonSound1.mp3
+https://s3.amazonaws.com/freecodecamp/simonSound2.mp3
+https://s3.amazonaws.com/freecodecamp/simonSound3.mp3
+https://s3.amazonaws.com/freecodecamp/simonSound4.mp3
