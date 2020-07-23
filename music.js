f49149538a9189d22243e2bb7adfc1c587a77e9b
Jem <jem@Julias-Air.home>
2020-07-23 16:16:19 -0400

    background music

---
+++ music.js
@@ -0,0 +1,12 @@
+var myAudio = document.getElementById("myAudio"); 
+
+
+$("#logo").click (startMusic);
+
+myAudio.volume = 0.1
+
+
+function startMusic() {
+    document.getElementById ("myAudio").play();
+
+}
