3dfbdfecd98a2c2035b9eacc30de728c8e416b00
swordcom367 <46449945+swordcom367@users.noreply.github.com>
2020-07-24 14:26:05 -0400

    Update music.js

--- music.js
+++ music.js
@@ -2,7 +2,7 @@ var myAudio = document.getElementById("myAudio");
 
 $(".startbutton").click(startMusic);
 
-myAudio.volume = 0.1;
+myAudio.volume = 0.2;
 
 function startMusic() {
     document.getElementById ("myAudio").play();
